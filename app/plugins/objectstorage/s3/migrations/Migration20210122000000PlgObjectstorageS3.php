b35834d6c6592c200fdbab2652438672c6ebaa54
Thomas Zajac <zajac.thomas1992@gmail.com>
2021-01-22 17:13:52 +0100

    S3 Storage initial commit, use mod_myresources and plg_filesystem/awss3 as base

---
+++ app/plugins/objectstorage/s3/migrations/Migration20210122000000PlgObjectstorageS3.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @package    hubzero-cms
+ * @copyright  Copyright 2005-2019 HUBzero Foundation, LLC.
+ * @license    http://opensource.org/licenses/MIT MIT
+ */
+
+use Hubzero\Content\Migration\Base;
+
+// No direct access
+defined('_HZEXEC_') or die();
+
+/**
+ * Migration script for adding Filesystem - AWS S3 plugin
+ **/
+class Migration20210122000000PlgObjectstorageS3 extends Base
+{
+	/**
+	 * Up
+	 **/
+	public function up()
+	{
+		$this->addPluginEntry('objectstorage', 's3', 0);
+	}
+
+	/**
+	 * Down
+	 **/
+	public function down()
+	{
+		$this->deletePluginEntry(',objectstorage', 's3');
+	}
+}
---
+++ app/plugins/objectstorage/s3/s3.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * @package    hubzero-cms
+ * @copyright  Copyright 2005-2019 HUBzero Foundation, LLC.
+ * @license    http://opensource.org/licenses/MIT MIT
+ */
+
+// No direct access
+defined('_HZEXEC_') or die();
+
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Storage;
+use Aws\S3\S3Client;
+use League\Flysystem\AwsS3v2\AwsS3Adapter;
+use League\Flysystem\Filesystem;
+
+/**
+ * Plugin class for AWS S3 filesystem connectivity
+ */
+class plgFilesystemAWSS3 extends \Hubzero\Plugin\Plugin
+{
+	/**
+	 * Initializes the AWS S3 connection
+	 *
+	 * @param   array   $params  Any connection params needed
+	 * @return  object
+	 **/
+	public static function init($params = [])
+	{
+		// Get the params
+		$pparams = Plugin::params('filesystem', 'awss3');
+
+		$app_id = $params['app_id'];
+		$app_secret = $params['app_secret'];
+		$region = $params['region'];
+		$bucket = $params['bucket'];
+		$path = isset($params['path']) ? $params['path'] : '';
+
+		$client = S3Client::factory([
+			'key'			=> $app_id,
+			'secret'	=> $app_secret,
+			'region'	=> $region,
+			'base_url' => 'http://s3.amazonaws.com',
+			'signature'=> 'v4'
+		]);
+		$adapter = new AwsS3Adapter($client, $bucket, $path);
+		return $adapter;
+	}
+}
